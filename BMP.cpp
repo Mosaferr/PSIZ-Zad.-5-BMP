0411772f468903998caa5872f278acdf3f03cbf4
Mosaferr <grzech.kowalczyk@gmail.com>
2022-06-23 16:26:11 +0200

    Dodanie bitmap i test.bmp

--- BMP.cpp
+++ BMP.cpp
@@ -2,10 +2,24 @@
 //Grzegorz Kowalczyk, D3
 
 #include <iostream>
+#include "bitmap_image.hpp"
 using namespace std;
 
 int main()
 {
-    cout << "Meta dane pliku z grafika bitmatowa: test.bmp: \n";
+    bitmap_image image("test.bmp");
+
+    if (!image)
+    {
+        cout << "\n Nie znaleziono pliku obrazu: test.bmp\n Program zostaje zamkniety. \n";
+        return 1;
+    }
+
+    cout << "----------------------------\n";
+    cout << " Dane pliku z grafika bitmatowa: test.bmp: \n";
+    cout << "  Szerokosc obrazu : " << image.width() << endl;
+    cout << "  Wysokosc obrazu: " << image.height() << endl;
+    cout << "  Liczba bajtow na piksel: " << image.bytes_per_pixel() << endl;
+    cout << "----------------------------\n";
     return 0;
 }
