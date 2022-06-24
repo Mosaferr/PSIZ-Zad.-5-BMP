ebf7ee9cff5f835f7f51d2ae77637303a2f238de
Mosaferr <grzech.kowalczyk@gmail.com>
2022-06-24 09:44:42 +0200

    Dodanie plików programu

--- BMP.cpp
+++ BMP.cpp
@@ -1,7 +1,9 @@
-﻿// BER
+﻿// BMP
 //Grzegorz Kowalczyk, D3
+// na podstawie: https://github.com/ArashPartow/bitmap
 
 #include <iostream>
+#include <vector>
 #include "bitmap_image.hpp"
 using namespace std;
 
@@ -11,15 +13,21 @@ int main()
 
     if (!image)
     {
-        cout << "\n Nie znaleziono pliku obrazu: test.bmp\n Program zostaje zamkniety. \n";
+        cout << "\n Nie znaleziono pliku obrazu: test.bmp\n Program zostaje zamkniety. \n\n";
+        //system("pause"); 
         return 1;
     }
+    image.invert_color_planes();
+    image.save_image("negatyw.bmp");
 
-    cout << "----------------------------\n";
-    cout << " Dane pliku z grafika bitmatowa: test.bmp: \n";
+    cout << "\n Tworzenie negatywu z obrazu bitmatowego \n";
+    for (int i = 0; i < 50; i++) cout << "-";
+    cout << "\n Dane pliku oryginalnego: test.bmp: \n";
     cout << "  Szerokosc obrazu : " << image.width() << endl;
     cout << "  Wysokosc obrazu: " << image.height() << endl;
-    cout << "  Liczba bajtow na piksel: " << image.bytes_per_pixel() << endl;
-    cout << "----------------------------\n";
+    cout << "  Liczba bajtow na piksel: " << image.bytes_per_pixel() << endl << endl;
+    cout << " Obraz w pliku test.bmp zostal przekonwertowany na negatyw i zapisany jako: negatyw.bmp \n";
+    for (int i = 0; i < 50; i++) cout << "-";
+    //system("pause");
     return 0;
 }
